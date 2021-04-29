fdd59238dd0d97c58cd146dd9344dea2f3c170eb
cadullms <cadullms@github.com>
2021-04-29 11:42:17 +0200

    unbound variable problem fixed

--- sync.sh
+++ sync.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 
-# set -Eeuo pipefail
-# trap cleanup SIGINT SIGTERM ERR EXIT
-
-# cleanup() {
-#   trap - SIGINT SIGTERM ERR EXIT
-#   # script cleanup here
-# }
-
-
-# script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+set -Eeuo pipefail
 
 # script logic here
 # AZCOPY_SOURCE_PATH - REQUIRED.
 # AZCOPY_DESTINATION_PATH - REQUIRED.
-# AZCOPY_DESTINATION_SAS - OPTIONAL. If not provided, this script will try to login via a managed identity. If the VM or pod this is running in, has a managed identity attached (in case of AKS, no PodIdentity needed, just a user-assigned or system-assigned identity) and if that identity has the needed permissions on the containers, then this will just work, without any token or credentials passed to the script.
+# AZCOPY_DESTINATION_SAS - OPTIONAL. If not provided, this script will try to login 
+#                          via a managed identity. If the VM or pod this is running in, 
+#                          has a managed identity attached (in case of AKS, no PodIdentity needed, 
+#                          just a user-assigned or system-assigned identity) and if that identity has 
+#                          the needed permissions on the containers, then this will just work,
+#                          without any token or credentials passed to the script.
 
-if [[ -z "${AZCOPY_DESTINATION_SAS}" ]]; then
-  echo "Starting the copy operation using managed identity (in case we have one attached)."
-  #TODO: Test the managed identity.
+# See https://docs.microsoft.com/en-us/azure/storage/common/storage-ref-azcopy-copy?toc=/azure/storage/blobs/toc.json
+if [[ -z "${AZCOPY_DESTINATION_SAS:-}" ]]; then
+  echo "Starting the copy operation using managed identity (in case there is one attached)."
   azcopy login --identity
-  azcopy sync "${AZCOPY_SOURCE_PATH}" "${AZCOPY_DESTINATION_PATH}" --recursive --put-md5
+  azcopy copy "${AZCOPY_SOURCE_PATH}" "${AZCOPY_DESTINATION_PATH}" --recursive --put-md5 --overwrite ifSourceNewer
 else
   echo "Starting the copy operation using the provided SAS token."
-  azcopy sync "${AZCOPY_SOURCE_PATH}" "${AZCOPY_DESTINATION_PATH}?${AZCOPY_DESTINATION_SAS}" --recursive --put-md5
+  azcopy copy "${AZCOPY_SOURCE_PATH}" "${AZCOPY_DESTINATION_PATH}?${AZCOPY_DESTINATION_SAS}" --recursive --put-md5 --overwrite ifSourceNewer
 fi
